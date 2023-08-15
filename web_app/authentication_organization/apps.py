fa401cd6949f5c2593410b98758579ea7d426a45
Ivanalq <aleqnikov@gmail.com>
2023-08-15 08:44:56 +0300

    first_commit

---
+++ web_app/authentication_organization/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthenticationOrganizationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authentication_organization'
