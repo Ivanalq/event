433b3e1f714aa6bfff4253cdd168ffe096a57da8
Ivanalq <aleqnikov@gmail.com>
2023-08-17 13:31:09 +0300

    third-commit

---
+++ web_app/base_app/static/base_app/js/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('#buttons')
+const navexit = document.querySelector('#exit-btn')
+const navbtn = document.querySelector('#nav-btn')
+
+navbtn.onclick = () => {
+    if (nav.classList.toggle('open')){
+
+    }
+}
+
+navexit.onclick = () => {
+    if (nav.classList.toggle('open')){
+
+    }
+}
+
+
